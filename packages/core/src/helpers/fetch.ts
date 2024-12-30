67eab5ef886ed4c50319bdf007856c862285c01d
Hudson S. Borges <hudsonsilbor@gmail.com>
2024-12-30 17:34:10 -0300

    fix(core): ajusta fetcher para adicionar statusCode para tratamento de erros

--- packages/core/src/helpers/fetch.ts
+++ packages/core/src/helpers/fetch.ts
@@ -1,4 +1,15 @@
 import fetchRetry from 'fetch-retry';
-import fetch from 'node-fetch';
+import fetch, { RequestInit } from 'node-fetch';
 
-export default fetchRetry(fetch);
+/**
+ *  Fetch with retry
+ *
+ * @param url
+ * @param options
+ * @returns
+ */
+export default async function (url: string | URL, options?: RequestInit) {
+  const response = await fetchRetry(fetch)(url, options);
+  if (response.status) Object.assign(response, { statusCode: response.status });
+  return response;
+}
