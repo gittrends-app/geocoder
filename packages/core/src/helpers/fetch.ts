899aa2049b0bed6d042b8137f7aac961fd4e96b9
Hudson Silva Borges <hudsonsilbor@gmail.com>
2025-12-26 18:54:28 -0300

    refactor: adiciona mais condições para novas tentativas

--- packages/core/src/helpers/fetch.ts
+++ packages/core/src/helpers/fetch.ts
@@ -9,7 +9,15 @@ import fetch, { RequestInfo, RequestInit } from 'node-fetch';
  * @returns
  */
 export default async function (url: string | URL | RequestInfo, options?: RequestInit) {
-  const response = await fetchRetry(fetch)(url, options);
+  const response = await fetchRetry(fetch)(url, {
+    retries: 3,
+    retryOn: (_, error, response) => {
+      return (
+        error != null || (response && (response.status === 429 || response.status >= 500)) || false
+      );
+    },
+    ...options
+  });
   if (response.status) Object.assign(response, { statusCode: response.status });
   return response;
 }
