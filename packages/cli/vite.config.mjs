392d57a4ce4711d70b6827875fb19cfbaf66cc57
Hudson Silva Borges <hudsonsilbor@gmail.com>
2026-09-06 17:57:46 -0400

    chore(release): v5.7.1

--- packages/cli/vite.config.mjs
+++ packages/cli/vite.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   resolve: {
+    alias: {
+      '@/core': fileURLToPath(new URL('../core/src/index.ts', import.meta.url))
+    },
     tsconfigPaths: true
   }
 });
